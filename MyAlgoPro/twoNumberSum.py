be1739c7d3011b0c3d7abf778d4762132d21ad4a
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-03-23 23:41:12 +0900

    p1

---
+++ MyAlgoPro/twoNumberSum.py
@@ -0,0 +1,47 @@
+# O(n^2) time / O(1) space
+def twoNumberSum1(array, targetSum):
+    for i in range(len(array) - 1):
+        firstNum = array[i]
+        for j in range(i + 1, len(array)):
+            secondNum = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []            
+
+# O(n) time / O(n) space 
+def twoNumberSum2(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
+        else:
+            nums[num] = True
+    return []                
+
+# O(nlog(n)) time / O(1) space
+def twoNumberSum3(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []                    
+  
+
+######################################################################  
+import program
+import unittest
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        output = program.twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)
+        self.assertTrue(len(output) == 2)
+        self.assertTrue(11 in output)
+        self.assertTrue(-1 in output)  
