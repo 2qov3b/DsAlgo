1d07af4263a66e31053caffa6eb27e0e90f375c1
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-03-24 23:27:50 +0900

    p1

---
+++ MyAlgoPro/isValidSubsequence.py
@@ -0,0 +1,31 @@
+# O(n) time / O(1) space - where n is the length of the array
+def isValidSubsequence1(array, sequence):
+    arrIdx = 0
+	seqIdx = 0
+	while arrIdx < len(array) and seqIdx < len(sequence):
+		if array[arrIdx] == sequence[seqIdx]:
+			seqIdx += 1
+		arrIdx += 1
+	return seqIdx == len(sequence)	
+
+# O(n) time / O(1) space - where n is the length of the array
+def isValidSubsequence2(array, sequence):
+    seqIdx = 0
+	for value in array:
+		if seqIdx == len(sequence):
+			break
+		if sequence[seqIdx] == value:
+			seqIdx += 1
+	return seqIdx == len(sequence)		
+
+
+
+######################################################################  
+import program
+import unittest
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        array = [5, 1, 22, 25, 6, -1, 8, 10]
+        sequence = [1, 6, -1, 10]
+        self.assertTrue(program.isValidSubsequence(array, sequence))
