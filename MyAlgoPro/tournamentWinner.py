b63a0aaf962b225b6681f192dbaec5afd8c866e8
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-03-27 20:48:59 +0900

    p1

---
+++ MyAlgoPro/tournamentWinner.py
@@ -0,0 +1,37 @@
+# O(n) time / O(k) space - where n is the # of competitions, k is the # of teams
+def tournamentWinner(competitions, results):
+	currentWinningTeam = ""
+	scores = {currentWinningTeam: 0}
+	
+    for idx, competition in enumerate(competitions):
+		winningResult = results[idx]
+		homeTeam, awayTeam = competition
+		
+		winningTeam = homeTeam if winningResult == 1 else awayTeam
+		
+		updateScores(winningTeam, 3, scores)
+		
+		if scores[winningTeam] > scores[currentWinningTeam]:
+			currentWinningTeam = winningTeam
+			
+    return currentWinningTeam
+
+def updateScores(team, points, scores):
+	if team not in scores:
+		scores[team] = 0
+	
+	scores[team] += points
+
+  
+######################################################################  
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        competitions = [["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]]
+        results = [0, 0, 1]
+        expected = "Python"
+        actual = program.tournamentWinner(competitions, results)
+        self.assertEqual(actual, expected)
