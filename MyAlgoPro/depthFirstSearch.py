97075cf0002ca810646f31a159a2358a0f2bdafd
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-03-31 13:31:41 +0900

    p1

---
+++ MyAlgoPro/depthFirstSearch.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+	
+    # O(v + e) time / O(v) space
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+		for child in self.children:
+			child.depthFirstSearch(array)
+		return array	
+  
+  
+  
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        graph = program.Node("A")
+        graph.addChild("B").addChild("C").addChild("D")
+        graph.children[0].addChild("E").addChild("F")
+        graph.children[2].addChild("G").addChild("H")
+        graph.children[0].children[1].addChild("I").addChild("J")
+        graph.children[2].children[0].addChild("K")
+        self.assertEqual(graph.depthFirstSearch([]), ["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"])
