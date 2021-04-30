8ecb6f02d83e83802b5f2913e453ff36ba0d2399
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-04-30 11:05:08 +0900

    p1

---
+++ MyAlgoPro/classPhotos.py
@@ -0,0 +1,30 @@
+# O(nlogn) time / O(1) space
+def classPhotos(redShirtHeights, blueShirtHeights):
+  redShirtHeights.sort(reverse=True)
+	blueShirtHeights.sort(reverse=True)
+	
+	firstRowShirt = 'RED' if redShirtHeights[0] < blueShirtHeights[0] else 'BLUE'
+	
+	for idx in range(len(redShirtHeights)):		
+		if firstRowShirt == 'RED':
+			if redShirtHeights[idx] >= blueShirtHeights[idx]:
+				return False
+		else:
+			if redShirtHeights[idx] <= blueShirtHeights[idx]:
+				return False
+			
+	return True		
+
+
+################################################
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        redShirtHeights = [5, 8, 1, 3, 4]
+        blueShirtHeights = [6, 9, 2, 4, 5]
+        expected = True
+        actual = program.classPhotos(redShirtHeights, blueShirtHeights)
+        self.assertEqual(actual, expected)
