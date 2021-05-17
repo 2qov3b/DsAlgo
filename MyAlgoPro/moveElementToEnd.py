7040b6de385c6be266782cf0b59cdbd708d15a9c
2qov3b <59160924+2qov3b@users.noreply.github.com>
2021-05-17 09:15:08 +0900

    p1

---
+++ MyAlgoPro/moveElementToEnd.py
@@ -0,0 +1,27 @@
+# O(n) time / O(1) space
+def moveElementToEnd(array, toMove):
+    i, j = 0, len(array) - 1
+	while i < j:
+		while i < j and array[j] == toMove:
+			j -= 1
+	    if array[i] == toMove:
+			array[i], array[j] = array[j], array[i]
+		i += 1
+	return array
+
+############################################
+import program
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        array = [2, 1, 2, 2, 2, 3, 4, 2]
+        toMove = 2
+        expectedStart = [1, 3, 4]
+        expectedEnd = [2, 2, 2, 2, 2]
+        output = program.moveElementToEnd(array, toMove)
+        outputStart = sorted(output[0:3])
+        outputEnd = output[3:]
+        self.assertEqual(outputStart, expectedStart)
+        self.assertEqual(outputEnd, expectedEnd)
